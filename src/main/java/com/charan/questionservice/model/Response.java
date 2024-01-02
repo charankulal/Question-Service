4adc66e00ed173c6291fc457970e0fe636ff966e
Charan <charankulal0241@gmail.com>
2024-01-02 19:37:33 +0530

    Completed Question service microservice

---
+++ src/main/java/com/charan/questionservice/model/Response.java
@@ -0,0 +1,13 @@
+package com.charan.questionservice.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+
+    private Integer id;
+    private String response;
+
+}
